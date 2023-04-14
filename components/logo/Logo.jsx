591809e28057a67f6512c82ef035a10e01c5cc4c
unknown <zeniqdev@gmail.com>
2023-04-14 10:59:34 +0400

    Fix: Console's Errors & Change the icons

--- components/logo/Logo.jsx
+++ components/logo/Logo.jsx
@@ -1,14 +1,16 @@
-import Link from "next/link"
 import zenconLogo from "../../assets/zenconLogo.svg"
 import Image from 'next/image'
 import styles from "./logo.module.css";
+import Link from "next/link";
+
 
 
 const Logo= ()=>{
     return <div className={`h-100 ${styles.brand__icon}`}>
-    <Link href="/">
-      <Image src={zenconLogo} alt="Logo"/>
-    </Link>
+      <Link href="#">
+        <><Image src={zenconLogo} alt="Logo"/></>
+      </Link>
   </div>  
 }
+
 export default Logo
