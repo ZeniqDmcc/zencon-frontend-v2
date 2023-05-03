0d5762b15f734b8e7e9e62f72ce4268242bd7ffe
unknown <zeniqdev@gmail.com>
2023-05-03 17:03:10 +0400

    Feat: Mobile screen issues are fixed.

--- components/Banner/index.jsx
+++ components/Banner/index.jsx
@@ -10,10 +10,10 @@ function Banner(props) {
           <div className="container">
             <div className="flex items-end justify-center h-screen">
                 <div className="flex flex-col items-center gap-4 lg:gap-10 sm:gap-5">
-                    <h1 className='px-2 text-2xl xl:text-4xl lg:text-5xl md:text-3xl sm:text-2xl text-center tracking-tighter text-green-400 z-10'>September 16 - 21, 2023</h1>
+                    <span className='px-2 text-2xl xl:text-4xl lg:text-5xl md:text-3xl sm:text-2xl text-center tracking-tighter text-green-400 z-10'>September 16 - 21, 2023</span>
                     <h1 className="w-3/5 text-2xl xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl text-center tracking-tighter text-white-400 z-10">{props.title}</h1>
                     <p className="text-lg xl:text-2xl md:text-xl sm:text-lg text-center sm:leading-8 leading-6 sm:max-w-7xl mx-auto block z-10">{props.description}</p>
-                    <div className="flex flex-col md:flex-row gap-4">
+                    <div className="flex flex-col md:flex-row sm:mt-0 gap-4 mt-2">
                         <Button label='Register / Login' href={redirectUrl}/>
                         <Button label='More Information' href="/more-information"/>
                     </div>
