0abe3c849511ff8a6a516caa73180b1b383f09af
Abdelfatah <zeniqdev@gmail.co>
2023-05-04 18:41:06 +0400

    remove unused code

--- utils/Constants/BackendURLs.js
+++ utils/Constants/BackendURLs.js
@@ -12,12 +12,6 @@ const URLs = {
         socketio_launchpadbackend: "https://staging.qinez.com/",
         zencon_event:"https://staging.qinez.com/api/v1/zencon"
     }
-    //  prod:{
-    //     idbackend: "https://idbackend.zeniq.id",
-    //     launchpadbackend: "http://192.168.19.100:8080/backend",
-    //     socketio_launchpadbackend: "http://192.168.19.100:8080",
-    //     zencon_event:"http://192.168.19.100:9003/api/v1/zencon"
-    // }
 }
 
 export const confirmation_page_endpoints = ["upload_project", "edit_project"];
@@ -25,5 +19,3 @@ export const confirmation_page_endpoints = ["upload_project", "edit_project"];
 export const api_backend = URLs.prod.launchpadbackend;
 export const socketio_backend = URLs.prod.socketio_launchpadbackend;
 export const zencon_event_api_url = URLs.prod.zencon_event;
-// export const api_backend = URLs.dev.api_rafi;
-// export const socketio_backend = URLs.dev.socketio_rafi;
