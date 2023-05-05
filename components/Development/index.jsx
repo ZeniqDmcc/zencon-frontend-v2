b2ad977a82ff20998e554440d49097f8b6d01db8
Abdelfatah <zeniqdev@gmail.co>
2023-05-05 14:01:50 +0400

    chore: adding images

--- components/Development/index.jsx
+++ components/Development/index.jsx
@@ -2,15 +2,18 @@ import Image from "next/image"
 import styles from "./development.module.css"
 import Button from "../Button"
 import { nanoid } from "nanoid"
-import graph4cr from "../../assets/graph4cr.png"
+// import graph4cr from "../../assets/graph4cr.png"
+import coin2 from "../../assets/coin2.png"
+import CoinMockup from "../../assets/CoinMockup.png"
 
 function Development(props) {
   return (
     <div className={styles.development}>
         <div className="container mx-auto">
             <div className="flex flex-col lg:flex-row justify-between gap-12">
-                <div className="flex-1 justify-center flex items-center lg:py-10" key={nanoid()}>
-                    {/* <Image src={props.image} alt="Nft" /> */}
+                <div className="flex-1 justify-center lg:justify-start flex items-center lg:py-10" key={nanoid()}>
+                    <Image src={props.image} alt="Nft" />
+                    <Image src={props.image2} alt="Nft" />
                 </div>
 
                 <div className="flex-1 flex flex-col lg:gap-12 gap-6" key={nanoid()}>
@@ -33,5 +36,6 @@ Development.defaultProps = {
     title: " Take Your Development Skills to the Next Level",
     shortDescription: "Are you ready to take your development skills to the next level? ZENCON Rio de Janeiro 2023 is the perfect opportunity to learn new skills, collaborate with other developers, and showcase your work on a global stage.",
     detailDescription: "With challenges that range from building advanced projects to migrating and integrating with the ZENIQ smartchain, the hackathon is the perfect opportunity to push the boundaries of what's possible with blockchain. Whether you're an experienced developer or just starting out, there's something for everyone at ZENCON Rio de Janeiro 2023.",
-    image: graph4cr
+    image: CoinMockup,
+    image2: coin2
 }
