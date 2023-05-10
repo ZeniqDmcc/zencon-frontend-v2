dff7884c35ca60fd4c1c862db808c2e09e0f5a59
unknown <zeniqdev@gmail.com>
2023-05-10 10:58:39 +0400

    Chore: bg Color and small correction

--- components/Finance/index.jsx
+++ components/Finance/index.jsx
@@ -7,9 +7,9 @@ function Finance(props) {
   return (
     <div className={`${styles.finance} xl:py-48 px-8`}>
         <div className="container mx-auto">
-            <div className="flex justify-between lg:gap-0 gap-0 flex-col lg:flex-row">
-                <div className="lg:flex-1 lg:order-1 order-2 flex flex-col lg:m-10" key={nanoid()}>
-                    <h2 className="xl:text-5xl lg:text-4xl md:text-2xl text-xl font-medium text-green-400 leading-16 lg:text-left text-center sm:py-8 pt-8 pb-0">{props.title}</h2>
+            <div className="flex justify-between aligns-center lg:gap-0 gap-0 flex-col lg:flex-row">
+                <div className="lg:flex-1 gap-12 lg:order-1 order-2 flex flex-col lg:m-10" key={nanoid()}>
+                    <h2 className="xl:text-5xl lg:text-4xl md:text-2xl text-xl font-medium text-green-400 leading-16 lg:text-left text-center sm:pt-0 pt-8 pb-0">{props.title}</h2>
                     <p className="w-90 leading-1.5 xl:text-2xl md:text-lg text-md font-normal lg:text-left text-center">{props.traditionalBanking}</p>
                     <p className="w-90 leading-1.5 xl:text-2xl md:text-lg text-md font-normal lg:text-left text-center">{props.challenges}</p>
                 </div>
--- components/Nft/index.jsx
+++ components/Nft/index.jsx
@@ -8,7 +8,7 @@ function Nft(props) {
     <div className={styles.nft}>
         <div className="container mx-auto">
           <div className="flex lg:flex-row flex-col lg:gap-0 gap-12 justify-between items-center pt-1">
-              <div className={`${styles.image} flex-1`} key={nanoid()}>
+              <div className={`${styles.image} flex-1 text-center`} key={nanoid()}>
                   <Image src={props.image} alt="Nft" />
               </div>
               <div className="flex-1 flex flex-col gap-12 lg:mt-0 sm:mt-2" key={nanoid()}>
